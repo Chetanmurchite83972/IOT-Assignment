2c6ac146d043bfd6066e30d352d4e00b6933d51a
Chetanmurchite <chetanmurchitte@gmail.com>
2024-04-18 23:08:32 +0530

    q1.py

---
+++ Day3/q1.py
@@ -0,0 +1,44 @@
+#Write a python program to add, delete, update employees into table
+
+# import mysql connector
+import mysql.connector
+
+# function to create a connection
+connection = mysql.connector.connect(
+    host = "localhost",
+    port = 3306,
+    user = "root",
+    password = "root",
+    database = "iotdb"
+)
+
+ # create a query
+empid = int(input("Enter empid : "))
+name = input("Enter name : ")
+department = (input("Enter department : "))
+email = input("Enter email :")
+salary = int(input("Enter salary : "))
+date_of_joining = input("Enter date of joining: ")
+
+query = f"insert into employee values({empid}, '{name}', '{department}', '{email}', {salary},'{date_of_joining}');"
+
+
+# create a cursor to execute the query
+cursor = connection.cursor()
+
+# execute query using cursor
+cursor.execute(query)
+
+# after execution of query commit your changes
+connection.commit()
+
+# close the cursor
+cursor.close()
+
+#close the connection with mysql server 
+connection.close()   
+
+
+
+
+
